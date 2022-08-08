bc33b00991cbe16d76f7396545ad55da45869b0c
elleven11 <federico.cassano@federico.codes>
2022-08-08 14:57:43 -0400

    doing some crazy dynamic linking hack

--- scripts/prepublishOnly.sh
+++ scripts/prepublishOnly.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -fr ./out/*
+
 # build z3 and put it into the bin folder, if z3 has not been built yet
 if [ ! -f "bin/z3" ]; then
   echo "Building z3..."
@@ -14,9 +16,13 @@ fi
 # build the rosette-solver and put it into the bin folder, if it has not been built yet
 if [ ! -d "bin/solver" ]; then
     echo "Building rosette-solver..."
-    pushd ../pacsolve/RosetteSolver
-    raco exe rosette-solver.rkt
-    raco distribute solver rosette-solver
-    popd
-    mv ../pacsolve/RosetteSolver/solver bin/
+    CURPATH=$(pwd)
+    PREV_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=bin/dlib/
+    pushd $CURPATH
+    raco exe --embed-dlls -o out/rosette-solver RosetteSolver/rosette-solver.rkt
+    raco distribute out/solver out/rosette-solver
+    mv out/solver bin/solver
+    pushd
+    export LD_LIBRARY_PATH=$PREV_LD_LIBRARY_PATH
 fi
